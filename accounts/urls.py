688aba6c9e958c18a6dcac937eff5e14a06ee80b
Azubuike <X00191169@myTUDublin.ie>
2021-12-08 22:10:53 +0000

    Fixed User Profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path
-from .views import UserEditView, ProfilePageView
-from django.urls import path
-from accounts.views import ProfilePageView, UserEditView
-from .views import SignupView, SigninView, SignoutView
+from .views import SignUpView, UserEditView, ProfilePageView
 
-app_name = 'accounts'
-
-urlpatterns = [
-    path('create/', SignupView, name='signup'),
-    path('login/', SigninView, name='signin'),
-    path('logout/', SignoutView, name='signout'),
+urlpatterns=[
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('edit_profile/<int:pk>/', UserEditView.as_view(), name='edit_profile'),
     path('profile/<int:pk>/', ProfilePageView.as_view(), name='show_profile'),
 ]
-
--- accounts/views.py
+++ accounts/views.py
@@ -1,61 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect
 from django.views.generic import CreateView, UpdateView, DetailView
 from .forms import CustomUserCreationForm
-from .models import CustomUser
-from django.contrib.auth.models import Group
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm
 from .models import Profile
-
 
 # Create your views here.
 
-def SignupView(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            signup_user = CustomUser.objects.get(username=username)
-            customer_group = Group.objects.get(name='Customer')
-            customer_group.user_set.add(signup_user)
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'registration/signup.html', {'form':form})
-
-
-
-def SigninView(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('shop:allProdCat')
-            else:
-                return redirect('signup')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'registration/signin.html', {'form':form})
-
-def SignoutView(request):
-    logout(request)
-    return redirect('signin')
-
-""" class SignUpView(CreateView):
+class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html' """
+    template_name = 'registration/signup.html'
 
 class UserEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Profile
     template_name = 'registration/edit_profile.html'
-    fields = ['user', 'profile_image', 'dob', 'address', 'phone', 'email']
+    fields = ['id', 'profile_image', 'dob', 'address', 'phone', 'email']
     success_url = reverse_lazy('home')
 
     def get_object(self):
@@ -64,7 +23,7 @@ class UserEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         return self.request.user.profile
 
-class ProfilePageView(DetailView, LoginRequiredMixin, UserPassesTestMixin):
+class ProfilePageView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
     success_url = reverse_lazy('home')
@@ -74,8 +33,4 @@ class ProfilePageView(DetailView, LoginRequiredMixin, UserPassesTestMixin):
 
     def test_func(self):
         return self.request.user.profile
-
-
-
-
 
